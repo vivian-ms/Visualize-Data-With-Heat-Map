f65c49441e76c5255cac15a34067a6edbfd5106f
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2021-03-27 17:51:37 -0700

    Created function to append svg element to #svg_container

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 const resource = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
+const w = 1250;
+const h = 400;
+const xPadding = 75;
+const yPadding = 80;
 
 
 window.onload = () => {
   fetch(resource)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
+      createCanvas(data);
     })
     .catch(err => {
       console.log(`Error: ${err}`);
     });
 };  // End window.onload()
+
+
+function createCanvas(data) {
+  let svg = d3.select('#svg_container')
+              .append('svg')
+              .attr('width', w + 2 * xPadding)
+              .attr('height',h + 2 * yPadding)
+              .append('g')
+              .attr('transform', `translate(${xPadding}, ${yPadding / 5})`);
+}
